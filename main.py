f8af46b998b77c66a794f6b4c6c0f38cf8f138d1
MiyuCode <streamlabsdonationtools@gmail.com>
2022-02-25 16:49:00 +0100

    feat: add features + translation in french + fix minor bugs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
-import tkinter.filedialog as fd
 from tkinter.scrolledtext import ScrolledText
+import tkinter.filedialog as fd
+import tkinter.messagebox as mb
 import platform
+
 filepath = ""
 
 def setfilepath(path):
@@ -10,49 +12,57 @@ def setfilepath(path):
 
 def openfile():
 	path = fd.askopenfilename()
-	if not path==():
+	if path == "":
+		mb.showerror("Tedit","Vous n'avez sélectionné aucun fichier texte !")
+	else:
 		with open(path, 'r') as f:
 			code = f.read()
 			editor.delete('1.0', END)
 			editor.insert("1.0", code)
 			setfilepath(path)
+			root.wm_title("Tedit - " + filepath)
 
 def savefile():
 	if filepath == "":
-		saveasfile()
+		mb.showerror("Tedit","Vous n'avez aucun nom à votre fichier texte !")
 	else:
 		with open(filepath, 'w') as f:
 			code = editor.get('1.0', END)
 			f.write(code)
+			root.wm_title("Tedit - " + filepath)
 
 def saveasfile():
 	path = fd.asksaveasfilename()
-	if not path==():
+	if path == "":
+		mb.showerror("Tedit","Vous n'avez aucun nom à votre fichier texte !")
+	else:
 		with open(path, 'w') as f:
 			code = editor.get('1.0', END)
 			f.write(code)
 			setfilepath(path)
+			root.wm_title("Tedit - " + filepath)
 
 root = Tk()
-root.wm_title("Tedit")
+root.wm_title("Tedit - Sans titre")
+root.iconbitmap("icons/tedit.ico")
 editor = ScrolledText(wrap=WORD, undo=True)
 editor.pack(fill=BOTH, expand=True)
 menubar = Menu(root)
 fileMenu = Menu(menubar, tearoff=False)
-fileMenu.add_command(label="Open", command=openfile)
-fileMenu.add_command(label="Save", command=savefile)
-fileMenu.add_command(label="Save As", command=saveasfile)
+fileMenu.add_command(label="Ouvrir", command=openfile)
+fileMenu.add_command(label="Enregistrer", command=savefile, accelerator="Ctrl+S")
+fileMenu.add_command(label="Enregistrer sous", command=saveasfile)
 fileMenu.add_separator()
-fileMenu.add_command(label="Exit", command=root.destroy)
+fileMenu.add_command(label="Quitter", command=root.destroy)
 editMenu = Menu(menubar, tearoff=False)
-editMenu.add_command(label="Cut", accelerator="Ctrl+X", command=lambda: editor.event_generate('<<Cut>>'))
-editMenu.add_command(label="Copy", accelerator="Ctrl+C", command=lambda: editor.event_generate('<<Copy>>'))
-editMenu.add_command(label="Paste", accelerator="Ctrl+V", command=lambda: editor.event_generate('<<Paste>>'))
+editMenu.add_command(label="Couper", accelerator="Ctrl+X", command=lambda: editor.event_generate('<<Cut>>'))
+editMenu.add_command(label="Copier", accelerator="Ctrl+C", command=lambda: editor.event_generate('<<Copy>>'))
+editMenu.add_command(label="Coller", accelerator="Ctrl+V", command=lambda: editor.event_generate('<<Paste>>'))
 editMenu.add_separator()
-editMenu.add_command(label="Undo", accelerator="Ctrl+Z", command=editor.edit_undo)
-editMenu.add_command(label="Redo", accelerator="Ctrl+Shift+Z", command=editor.edit_redo)
-menubar.add_cascade(label="File", menu=fileMenu)
-menubar.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Annuler", accelerator="Ctrl+Z", command=editor.edit_undo)
+editMenu.add_command(label="Refaire", accelerator="Ctrl+Shift+Z", command=editor.edit_redo)
+menubar.add_cascade(label="Fichier", menu=fileMenu)
+menubar.add_cascade(label="Éditer", menu=editMenu)
 root.config(menu=menubar)
 root.bind()
 root.mainloop()
